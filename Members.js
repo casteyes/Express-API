8d16120291dde1bc635e0cfdfec3b102e911f802
Casteyes <casteyestothesun@gmail.com>
2021-07-04 19:33:33 -0400

    initial commit

---
+++ Members.js
@@ -0,0 +1,18 @@
+const members = [{
+    id:1,
+    name: 'John Doe', 
+    email:'john@gmail.com',
+    status:'active'},
+        {
+            id:2,
+            name: 'Gohn Doe',
+            email:'gohn@gmail.com',
+            status:'inactive'},
+        {
+            id:3,
+            name: 'Rohn Doe', 
+            email:'rohn@gmail.com',
+            status:'active'},
+    ];
+
+    module.exports = members;
